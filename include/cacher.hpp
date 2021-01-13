585cdffaff2b4f3041203fdd213732cc419494cd
FORTYSS <dmitriy_sviridov_2001@mail.ru>
2021-01-13 23:43:41 +0300

    CMake OK

---
+++ include/cacher.hpp
@@ -0,0 +1,13 @@
+// Copyright 2020 by FORTYSS
+
+#ifndef INCLUDE_CACHER_HPP_
+#define INCLUDE_CACHER_HPP_
+
+#include <chrono>
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <set>
+#include <string>
+
+#endif // INCLUDE_CACHER_HPP_
---
+++ sources/cacher.cpp
@@ -0,0 +1,3 @@
+// Copyright 2020 by FORTYSS
+
+#include <cacher.hpp>
